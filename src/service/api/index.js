f1e92baa17299f3752ae1c83cdeebe6786e20791
Dmitriy <d.orlov777@gmail.com>
2020-06-11 13:38:47 +0300

    refactor

--- src/service/api/index.js
+++ src/service/api/index.js
@@ -1,22 +1,6 @@
 'use strict';
 
-const {Router} = require(`express`);
-const {DataService} = require(`../data-service`);
-const {getMockPosts} = require(`../../utils`);
-const {createCategoryRouter} = require(`./create-category-router`);
-const {createSearchRouter} = require(`./create-search-router`);
-const {createPostRouter} = require(`./create-post-router`);
-
-const createAPI = async () => {
-  const dataService = new DataService(await getMockPosts());
-  const router = new Router();
-
-  router.use(`/articles`, createPostRouter(dataService));
-  router.use(`/categories`, createCategoryRouter(dataService));
-  router.use(`/search`, createSearchRouter(dataService));
-
-  return router;
-};
+const {createAPI} = require(`./create-api`);
 
 module.exports = {
   createAPI
