0f2a9216a4d22f1d513cd39b51755c9b1217484b
Dmitriy <d.orlov777@gmail.com>
2020-08-27 14:21:45 +0300

    add validation to main router

--- src/express/routes/main.js
+++ src/express/routes/main.js
@@ -3,6 +3,7 @@
 const {Router} = require(`express`);
 const {DataServer} = require(`../data-server`);
 const {getPagination} = require(`../utils`);
+const {getCategories} = require(`../middlewares`);
 const {
   POST_PREVIEW_COUNT,
   LASTST_COMMENT_COUNT,
@@ -11,6 +12,7 @@ const {
   COMMENT_PREVIEW_LENGTH,
   PostSortType,
 } = require(`../const`);
+const Joi = require(`joi`);
 
 const reduceText = (text, length) => {
   return text.length > length ? `${text.slice(0, length)}...` : text;
@@ -20,42 +22,46 @@ const mainRouter = new Router();
 const dataServer = new DataServer();
 
 mainRouter.get(`/`, async (req, res, next) => {
-  let categories;
-  let popularPosts;
-  let posts;
-  let postCount;
-  let comments;
-  const page = Number(req.query.page) || 1;
-
   try {
-    [categories, {posts: popularPosts}, {posts, postCount}, comments] = await Promise.all([
+    const {posts: popularPosts, postCount} = await dataServer.getPostPreviews(PostSortType.POPULARITY, POPULAR_POST_COUNT, 0);
+    const pageCount = Math.ceil(Number(postCount) / POST_PREVIEW_COUNT);
+    const pageSchema = Joi.object({
+      page: Joi.number().min(1).max(pageCount).optional()
+    });
+
+    await pageSchema.validateAsync(req.query);
+    const page = Number(req.query.page) || 1;
+
+    const [categories, {posts}, comments] = await Promise.all([
       dataServer.getCategories(true),
-      dataServer.getPostPreviews(PostSortType.POPULARITY, POPULAR_POST_COUNT, 0),
       dataServer.getPostPreviews(PostSortType.DATE, POST_PREVIEW_COUNT, (page - 1) * POST_PREVIEW_COUNT),
       dataServer.getComments(LASTST_COMMENT_COUNT, 0),
     ]);
+
+    res.render(`main`, {
+      categories,
+      posts,
+      popularPosts: popularPosts.filter((it) => it[`comment_count`] > 0).map((it) => ({
+        ...it,
+        announce: reduceText(it.announce, ANNOUNCE_PREVIEW_LENGTH),
+      })),
+
+      comments: comments.map((it) => ({
+        ...it,
+        text: reduceText(it.text, COMMENT_PREVIEW_LENGTH),
+      })),
+
+      pagination: getPagination(page, pageCount, req.path),
+    });
   } catch (err) {
+    if (err.isJoi) {
+      res.render(`400`);
+      return;
+    }
+
     next(err);
     return;
   }
-
-  const pageCount = Math.ceil(Number(postCount) / POST_PREVIEW_COUNT);
-
-  res.render(`main`, {
-    categories,
-    posts,
-    popularPosts: popularPosts.filter((it) => it[`comment_count`] > 0).map((it) => ({
-      ...it,
-      announce: reduceText(it.announce, ANNOUNCE_PREVIEW_LENGTH),
-    })),
-
-    comments: comments.map((it) => ({
-      ...it,
-      text: reduceText(it.text, COMMENT_PREVIEW_LENGTH),
-    })),
-
-    pagination: getPagination(page, pageCount, req.path),
-  });
 });
 
 module.exports = {
