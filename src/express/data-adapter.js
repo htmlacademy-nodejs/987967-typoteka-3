3de09d4d72cc201692c27f8d76b4b843b7a32926
Dmitriy <d.orlov777@gmail.com>
2020-08-27 16:03:18 +0300

    refactor

--- src/express/data-adapter.js
+++ src/express/data-adapter.js
@@ -2,8 +2,6 @@
 
 const {formatDateTime, formatDate} = require(`./utils`);
 
-const CATEGORY_ID_PREFIX = `category-id-`;
-
 const ServiceToExpressAdapter = {
   getComment(rawComment) {
     const date = new Date(rawComment.date);
@@ -38,27 +36,6 @@ const ServiceToExpressAdapter = {
   }
 };
 
-const ExpressToServiceAdapter = {
-  getPost(postData) {
-    const regexp = RegExp(`${CATEGORY_ID_PREFIX}(.+)`);
-
-    const categories = Object.keys(postData).reduce((acc, cur) => {
-      const matches = cur.match(regexp);
-      return matches ? [...acc, matches[1]] : acc;
-    }, []);
-
-    const [day, month, year, hour, minute] = postData.date.replace(/[, :]+/g, `.`).split(`.`).map((it) => Number(it));
-    const date = new Date(year, month - 1, day, hour, minute).toISOString();
-
-    return {
-      ...postData,
-      categories,
-      date,
-    };
-  }
-};
-
 module.exports = {
   ServiceToExpressAdapter,
-  ExpressToServiceAdapter,
 };
