5e3d1dad7d08e435ff2a217afcc9e18ea8ddd185
Dmitriy <d.orlov777@gmail.com>
2020-10-12 18:09:14 +0300

    service: remove pg from dependency

--- src/service/utils/db.js
+++ src/service/utils/db.js
@@ -1,8 +1,7 @@
 'use strict';
 
-const fs = require(`fs`);
-const {Client} = require(`pg`);
-const {ADMIN, PSW, POSTGRES_PSW, HOST, DB_PORT} = require(`../config`);
+const {Sequelize} = require(`sequelize`);
+const {ADMIN, POSTGRES_PSW, HOST} = require(`../config`);
 
 const getLimitConstrain = (limit, offset = 0) => {
   return limit ? {
@@ -11,106 +10,39 @@ const getLimitConstrain = (limit, offset = 0) => {
   } : {};
 };
 
-const createSchemaCommands = (file) => fs.readFileSync(file).toString().split(`;`).map((it) => `${it.trim()};`);
+const createRootSequelize = () => new Sequelize(`postgres`, `postgres`, POSTGRES_PSW, {
+  host: HOST,
+  dialect: `postgres`,
+  logging: false,
+});
 
-const dropDataBase = async (dbName) => {
-  const client = new Client({
-    user: `postgres`,
-    host: HOST,
-    database: `postgres`,
-    password: POSTGRES_PSW,
-    port: DB_PORT
+const createDatabase = async (dbName) => {
+  const sequelize = createRootSequelize();
+
+  const queryInterface = sequelize.getQueryInterface();
+
+  await queryInterface.dropDatabase(dbName);
+
+  await queryInterface.createDatabase(dbName, {
+    collate: `C`,
+    encoding: `UTF8`,
+    ctype: `C`,
+    template: `template0`,
   });
 
-  try {
-    await client.connect();
-    await client.query(`DROP DATABASE IF EXISTS ${dbName};`);
-  } catch (err) {
-    client.end();
-    throw err;
-  }
+  await sequelize.query(`ALTER DATABASE ${dbName} OWNER TO ${ADMIN};`);
 
-  await client.end();
+  sequelize.close();
 };
 
-const createEmptyDataBase = async ({dbName, owner, host, port, postgresPassword}) => {
-  const client = new Client({
-    user: `postgres`,
-    host,
-    database: `postgres`,
-    password: postgresPassword,
-    port
-  });
-
-  try {
-    await client.connect();
-    await client.query(`DROP DATABASE IF EXISTS ${dbName};`);
-    await client.query(`
-    CREATE DATABASE ${dbName}
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'C'
-    LC_CTYPE = 'C'
-    TABLESPACE = pg_default
-    TEMPLATE template0
-    CONNECTION LIMIT = -1;`);
-
-    await client.query(`ALTER DATABASE ${dbName} OWNER TO ${owner};`);
-  } catch (err) {
-    client.end();
-    throw err;
-  }
-
-  await client.end();
-};
-
-const createSchema = async ({dbName, admin, password, host, port}) => {
-  const client = new Client({
-    user: admin,
-    host,
-    database: dbName,
-    password,
-    port
-  });
-
-  const commands = createSchemaCommands(`${process.cwd()}/sql/create-schema.sql`);
-  try {
-    await client.connect();
-    for (let i = 0; i < commands.length; i++) {
-      await client.query(commands[i]);
-    }
-  } catch (err) {
-    client.end();
-    throw err;
-  }
-
-  client.end();
-};
-
-const createDataBase = async (dbName) => {
-  await createEmptyDataBase({
-    dbName,
-    owner: ADMIN,
-    host: HOST,
-    port: DB_PORT,
-    postgresPassword: POSTGRES_PSW
-  });
-
-  await createSchema({
-    dbName,
-    admin: ADMIN,
-    password: PSW,
-    host: HOST,
-    port: DB_PORT
-  });
+const dropDatabase = async (dbName) => {
+  const sequelize = createRootSequelize();
+  await sequelize.queryInterface.dropDatabase(dbName);
+  sequelize.close();
 };
 
 module.exports = {
   getLimitConstrain,
-  createSchemaCommands,
-  createEmptyDataBase,
-  createSchema,
-  createDataBase,
-  dropDataBase,
+  createDatabase,
+  dropDatabase,
 };
