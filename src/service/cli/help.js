96de5fce509fd1ef4b2cacab56df5371e0f9a0d2
Dmitriy <d.orlov777@gmail.com>
2020-10-18 14:07:24 +0300

    service: change cli messages

--- src/service/cli/help.js
+++ src/service/cli/help.js
@@ -13,7 +13,7 @@ module.exports = {
   },
 
   run() {
-    const helpMessage = `Гайд:\n\tservice <command>\n\n\tКоманды:\n\t\t${this.message}\n\nПример создания БД: npm run start:service -- --generate-db 50 10\n\nДеплой: https://typoteka.shgk.me`;
+    const helpMessage = `Гайд:\n\tservice <command>\n\n\tКоманды:\n\t\t${this.message}\n\nПример создания БД: npm run start:service -- --generate-db 50 10\n\nЗапуск приложения: npm run start\n\nДеплой: https://typoteka.shgk.me, админ: isabelle.thoasm@gmail.com / obehselImsaaTl`;
 
     console.info(chalk.grey(helpMessage));
     return ExitCode.SUCCESS;
